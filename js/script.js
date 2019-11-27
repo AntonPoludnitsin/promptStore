81441c8848acdefc691a26cb70ddefb56e14ebda
AntonPoludnitsin <33234187+AntonPoludnitsin@users.noreply.github.com>
2019-11-27 17:05:26 +0400

    added  change

--- js/script.js
+++ js/script.js
@@ -1,16 +1,1 @@
-const value = menu.actionList()
-if (value === "1") {
-  goods.addedCart();  
-} else if (value === "2"){
-  cart.actionList();
-}
-
-
-
-
-
-
-/* console.log(cart.cart);
-goods.addedCart();
-
-console.log(cart.cart) */
+menu.show();
