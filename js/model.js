83d32cdc2b384f6d8c12823d6deb6936e4400796
AntonPoludnitsin <33234187+AntonPoludnitsin@users.noreply.github.com>
2019-12-17 16:05:50 +0400

    added model file

--- js/model.js
+++ js/model.js
@@ -1,3 +1,28 @@
-let goods = [];
+const data = [{
+  title: "Товар1",
+  country: "USA",
+  widt: 400,
+  height: 300,
+  weight: 200
+},
+{
+  title: "Товар2",
+  country: "Europe",
+  widt: 500,
+  height: 350,
+  weight: 500
+}];
 
 let modelCart = [];
+
+let cartEnding = [{
+  name: 'Оплата',
+  action: function () {
+    this.pay();
+  }
+},
+{
+  name: 'Вернуться на главную страницу',
+  action: () => menu.show()
+}
+];
